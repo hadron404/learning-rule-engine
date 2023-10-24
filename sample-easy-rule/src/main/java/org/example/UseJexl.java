9ddd3b3ce2e6a6770b3229a04ad74727ad543c37
zhouqiang <2902511578@qq.com>
2023-10-24 16:06:44 +0800

    how-to:easy-rule中表达式引擎的用法

---
+++ sample-easy-rule/src/main/java/org/example/UseJexl.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.jeasy.rules.api.Facts;
+import org.jeasy.rules.api.Rule;
+import org.jeasy.rules.api.Rules;
+import org.jeasy.rules.api.RulesEngine;
+import org.jeasy.rules.core.DefaultRulesEngine;
+import org.jeasy.rules.jexl.JexlRule;
+
+public class UseJexl {
+	public void test() {
+		Rule rule = new JexlRule()
+			.name("myRule")
+			.description("myRuleDescription")
+			.priority(3)
+			.when("c == a");
+		Rules rules = new Rules();
+		rules.register(rule);
+
+		Facts facts = new Facts();
+		facts.put("c", "a");
+		RulesEngine rulesEngine = new DefaultRulesEngine();
+		rulesEngine.fire(rules, facts);
+	}
+}
