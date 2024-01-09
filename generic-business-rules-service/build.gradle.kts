8bdd9ffb8271c069dd97f0601f36dd26e6564ac3
zhouqiang <2902511578@qq.com>
2024-01-09 17:43:45 +0800

    继承JPA，MySQL

--- generic-business-rules-service/build.gradle.kts
+++ generic-business-rules-service/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     java
     id("org.springframework.boot") version "3.1.4"
@@ -10,9 +9,15 @@ apply(plugin = "io.spring.dependency-management")
 
 dependencies {
 
+//    jpa 支持
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//    mysql 支持
+    implementation("com.mysql:mysql-connector-j:8.2.0")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
     implementation("org.jeasy:easy-rules-core:4.1.0")
     implementation("org.jeasy:easy-rules-jexl:4.1.0")
 
--- generic-business-rules-service/src/main/java/org/example/domain/model/rule/BusinessRuleRepository.java
+++ generic-business-rules-service/src/main/java/org/example/domain/model/rule/BusinessRuleRepository.java
@@ -1,13 +1,10 @@
 package org.example.domain.model.rule;
 
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 /**
  * 业务规则资源库
  */
-@Repository
 public interface BusinessRuleRepository {
 
 
