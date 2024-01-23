22aab0baee08a8d3907a32b1474e22bcd718e226
zhouqiang <2902511578@qq.com>
2024-01-23 09:42:59 +0800

    restructure: 重新组织功能类

--- generic-business-rules-service/src/main/java/org/example/application/media/RequestFactsWevMvcConfiguration.java
+++ generic-business-rules-service/src/main/java/org/example/application/media/RequestFactsWevMvcConfiguration.java
@@ -1,9 +1,16 @@
 package org.example.application.media;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import jakarta.annotation.Nonnull;
 import jakarta.servlet.http.HttpServletRequest;
+import org.example.infrastructure.InitializationBeanConfiguration;
+import org.jeasy.rules.api.Fact;
+import org.jeasy.rules.api.Facts;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.MethodParameter;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -11,12 +18,19 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+import java.util.Map;
 
 @Configuration
-class RequestFactsResolverConfiguration implements WebMvcConfigurer {
+class RequestFactsWevMvcConfiguration implements WebMvcConfigurer {
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addConverter(new RequestFactsConverter());
+		WebMvcConfigurer.super.addFormatters(registry);
+	}
+
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-		resolvers.add(new RequestFactsResolverConfiguration.ContextCommandHandlerMethodArgumentResolver());
+		resolvers.add(new RequestFactsWevMvcConfiguration.ContextCommandHandlerMethodArgumentResolver());
 		WebMvcConfigurer.super.addArgumentResolvers(resolvers);
 	}
 
@@ -36,6 +50,25 @@ class RequestFactsResolverConfiguration implements WebMvcConfigurer {
 			// 	.readTree(Base64.getDecoder().decode(request.getQueryString()));
 
 			return null;
+		}
+	}
+
+	// @Component
+	public static class RequestFactsConverter implements Converter<String, Facts> {
+		@Override
+		public Facts convert(@Nonnull String value) {
+			try {
+				Facts facts = new Facts();
+				Map<String, ?> result = InitializationBeanConfiguration.objectMapper()
+					.readValue(value, new TypeReference<>() {
+					});
+				result.entrySet().stream()
+					.map(i -> new Fact<>(i.getKey(), i.getValue()))
+					.forEach(facts::add);
+				return facts;
+			} catch (JsonProcessingException e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
 }
